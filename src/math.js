1f92952e37ef3ace3d52837ef2a1664288bfd7f4
viktoria <kingsviktory@gmail.com>
2024-01-30 00:01:33 +0300

    fix: number toFixed without unnecessary zeros

--- src/math.js
+++ src/math.js
@@ -1,12 +1,14 @@
-export const sum = (a, b) => parseFloat(a) + parseFloat(b);
-export const substraction = (a, b) => parseFloat(a) - parseFloat(b);
-export const multiply = (a, b) => parseFloat(a) * parseFloat(b);
+const toFixedWithoutZeros = (num, precision = 8) => Number.parseFloat(num.toFixed(precision));
+
+export const sum = (a, b) => toFixedWithoutZeros(parseFloat(a) + parseFloat(b));
+export const substraction = (a, b) => toFixedWithoutZeros(parseFloat(a) - parseFloat(b));
+export const multiply = (a, b) => toFixedWithoutZeros(parseFloat(a) * parseFloat(b));
 export const division = (a, b) => {
   if (Number.isFinite(parseFloat(a) / parseFloat(b))) {
     if (Number.isInteger(parseFloat(a) / parseFloat(b))) {
       return parseFloat(a) / parseFloat(b);
     }
-    return (parseFloat(a) / parseFloat(b)).toFixed(8);
+    return toFixedWithoutZeros((parseFloat(a) / parseFloat(b)));
   }
   return "Error";
 };
